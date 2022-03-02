2fe11d819ab6e4d760fa5bb512e82dba92099227
vrishtimalik <vrishtimalik2001@gmail.com>
2022-03-03 02:24:45 +0530

    update

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,7 +2,7 @@ import React from "react"
 import Layout from "../components/Layout/Layout"
 
 const Contact = () => {
-  return <Layout>Contact</Layout>)
+  return <Layout>Contact</Layout>
 }
 
 export default Contact
